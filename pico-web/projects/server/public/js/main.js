e2b5f8492fbba18293e6d59e7c3beddb6cf7d7da
ddb_tribal_reggie_senal <reggie.senal@sg.tribalworldwide.com>
2023-02-06 17:42:14 +0800

    setup LED ajax call

--- pico-web/projects/server/public/js/main.js
+++ pico-web/projects/server/public/js/main.js
@@ -1,1 +1,33 @@
 console.log("main js");
+$(function () {
+
+    const $btnon = $('#btnon');
+    const $btnoff = $('#btnoff');
+    const $led = $('#led');
+    
+    function sendPostLed(v) {
+        console.log("v", v);
+        $.post('/LED',
+            { led: v })
+            .done(function (data) {
+                console.log('data', data);
+                let led = data.led;
+                $led.removeClass('red');
+                if (led == 'on'){
+                    $led.addClass('red');
+                }
+            });
+    }
+
+    $btnon.click(function (e) {
+        e.preventDefault();
+        console.log("on clicked");
+        sendPostLed($btnon.val());
+    });
+
+    $btnoff.click(function (e) {
+        e.preventDefault();
+        console.log("off clicked");
+        sendPostLed($btnoff.val());
+    });
+});
